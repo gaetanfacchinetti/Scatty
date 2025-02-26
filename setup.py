d8ea122404322f89b1b39a75c2eb68f1668c2085
Gaétan Facchinetti <gaetanfacc@gmail.com>
2025-02-26 15:28:49 +0100

    cleaned again

--- setup.py
+++ setup.py
@@ -1,57 +1,8 @@
 import subprocess
 import os
-
-from setuptools import setup, Extension
-from Cython.Build import cythonize
-
-"""
-def main():
-
-    # go to where the project needs to be compiled
-    
-    os.chdir("./src/scatty/src/")
-
-    try:
-
-        if os.path.exists("libcscatty.so"):
-            os.remove("libcscatty.so")
-
-        subprocess.run(['make'], check=True)
-        print("make executed successfully.")
-
-        # Run the 'make' command with the desired target
-        subprocess.run(['make', 'clean'], check=True)
-        print("make clean executed successfully.")
-
-
-    except subprocess.CalledProcessError as e:
-        print("Error executing Makefile:", e)
-
-    # go back to the main project folder
-    os.chdir("../../../")
-    
-    # cytonize the cython code
-    cythonize("./src/scatty/wrapper.pyx", force = True)
-
-    # define the extension for the libraries
-    extension = Extension("scatty.wrapper", 
-                        sources = ["./src/scatty/wrapper.c"], 
-                        libraries = ["cscatty", "m"], 
-                        library_dirs=["./src/scatty/src/"],)
-    
-    setup(ext_modules=[extension], 
-          package_dir={'' : 'src'},  
-          packages=['scatty'], 
-          package_data= {'scatty' : ['data/*', 'src/*.h', 'src/*.so']},
-          include_package_data=True)
-
-
-if __name__ == "__main__":
-    main()
-"""
+from setuptools import setup
 
 from distutils.command.install import install as _install
-
 
 class install(_install):
     def run(self):
